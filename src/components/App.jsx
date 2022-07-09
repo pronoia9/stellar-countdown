53ef03d127c0e87663477a410a0ed532c12fa563
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-08 21:49:11 -0400

    change date prop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,11 @@ import Logos from './Logos/Logos';
 import Background from './Background/Background';
 
 export default function App() {
+  const date = new Date(new Date().setSeconds(new Date().getSeconds() + (691200 + 82800 + 3300 + 44)));
   return (
     <div className=''>
       <Header title={`We're launching soon`.toUpperCase()} />
-      <Countdown date={new Date(new Date().setSeconds(new Date().getSeconds() + (691200 + 82800 + 3300 + 44)))} />
+      <Countdown date={`${date.getDate()}, ${date.getHours()}, ${date.getMinutes()}, ${date.getSeconds()}`.split(',')} />
       <Logos />
       <Background />
     </div>
