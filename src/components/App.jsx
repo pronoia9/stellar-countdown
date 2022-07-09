280c03dc5e4a88d1d5e97374bec70c75306f48cc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-08 21:06:38 -0400

    import components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,8 @@
 import './App.scss';
+import Header from './Header/Header';
+import Cards from './Cards/Cards';
+import Logos from './Logos/Logos';
+import Background from './Background/Background';
 
 export default function App() {
 
