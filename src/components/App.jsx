b2ec268f96bb2c159ea557e809893d5b7609e833
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-08 20:52:39 -0400

    remove context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,6 @@
 import './App.scss';
-import React, { useContext } from 'react';
-import { ThemeContext } from '../context/ThemeContext';
 
 export default function App() {
-  const theme = useContext(ThemeContext);
-  const darkMode = theme.state.darkMode;
 
-  return <div className={`${darkMode ? 'bg-dark' : 'bg-light'}`}></div>;
+  return <div className=''></div>;
 }
