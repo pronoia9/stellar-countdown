775fcf13a62660488f542254653de77e204a8e2c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-03 22:42:23 +0300

    get date prop, import card

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -1,5 +1,11 @@
 import './Countdown.scss';
+import Card from './Card';
 
-export default function Countdown() {
-  return <></>;
+export default function Countdown({ date }) {
+  return <div className='countdown'></div>;
 }
+
+// Days;
+// Hours;
+// Minutes;
+// Seconds;
