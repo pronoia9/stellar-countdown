f51bd773c7d05f4264798422ba42bbbd8674ac38
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-05 19:29:45 +0300

    finish card component

--- src/components/Countdown/Card.jsx
+++ src/components/Countdown/Card.jsx
@@ -1,5 +1,13 @@
 import './Card.scss';
 
-export default function Card() {
-  return <></>;
+export default function Card({ time, countdown }) {
+  return (
+    <div className='card-flip'>
+      <span className='card-number'>
+        <span className='card-number-top' />
+        {countdown}
+      </span>
+      <p>{time}</p>
+    </div>
+  );
 }
