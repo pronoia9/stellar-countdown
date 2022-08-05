1c2f083e8f2e500b80e6176152d035569d068168
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-05 21:45:56 +0300

    add fontawesome icons

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -1,5 +1,17 @@
 import './Social.scss';
 
 export default function Social() {
-  return <></>;
+  return (
+    <div className='social'>
+      <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>
+        <i class='fa-brands fa-square-facebook'></i>
+      </a>
+      <a href='http://www.pinterest.com/' target='_blank' rel='noreferrer'>
+        <i class='fa-brands fa-pinterest'></i>
+      </a>
+      <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
+        <i class='fa-brands fa-instagram'></i>
+      </a>
+    </div>
+  );
 }
