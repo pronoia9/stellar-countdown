443071b46ac7a77628f206e6338ef1e80e004798
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-05 19:19:55 +0300

    move date to countdown

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,10 @@ import Logos from './Logos/Logos';
 import Background from './Background/Background';
 
 export default function App() {
-  const date = new Date(new Date().setSeconds(new Date().getSeconds() + (691200 + 82800 + 3300 + 44)));
   return (
     <div className=''>
       <Header title={`We're launching soon`.toUpperCase()} />
-      <Countdown date={`${date.getDate()}, ${date.getHours()}, ${date.getMinutes()}, ${date.getSeconds()}`.split(',')} />
+      <Countdown />
       <Logos />
       <Background />
     </div>
