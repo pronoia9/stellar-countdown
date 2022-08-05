a887c8add4cf5fa596034e9443b40fffe33a73c4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-05 19:24:13 +0300

    add launch date + timeLeft func

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -1,11 +1,26 @@
 import './Countdown.scss';
 import Card from './Card';
+import { useEffect, useState } from 'react';
 
 export default function Countdown({ date }) {
-  return <div className='countdown'></div>;
-}
+  const timeLeft = () => {
+    const time = +date - +new Date();
+    return {
+      days: Math.floor(time / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((time / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((time / 1000 / 60) % 60),
+      seconds: Math.floor((time / 1000) % 60),
+    };
+  }
 
-// Days;
-// Hours;
-// Minutes;
-// Seconds;
+  const [countdown, setCountdown] = useState(timeLeft());
+
+  return (
+    <div className='countdown'>
+      <p>{countdown.days}</p>
+      <p>{countdown.hours}</p>
+      <p>{countdown.minutes}</p>
+      <p>{countdown.seconds}</p>
+    </div>
+  );
+}
