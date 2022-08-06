ea5acc33fef73b61ac68d9df719be4e50b9e1ad8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-06 17:43:48 +0300

    implement flipping clock card

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -1,27 +1,75 @@
+import React from 'react';
+import CountdownCard from './CountdownCard';
+import './FlipClock.scss';
 import './Countdown.scss';
-import Card from './Card';
-import { useEffect, useState } from 'react';
 
-export default function Countdown({ date }) {
-  const timeLeft = () => {
-    const time = +date - +new Date();
-    return {
-      days: Math.floor(time / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((time / (1000 * 60 * 60)) % 24),
-      minutes: Math.floor((time / 1000 / 60) % 60),
-      seconds: Math.floor((time / 1000) % 60),
+// class component
+class Countdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      daysShuffle: true,
+      hours: 0,
+      hoursShuffle: true,
+      minutes: 0,
+      minutesShuffle: true,
+      seconds: 0,
+      secondsShuffle: true,
     };
-  };
+  }
 
-  const [countdown, setCountdown] = useState(timeLeft());
-  useEffect(() => { setTimeout(() => { setCountdown(timeLeft()); }, 1000); });
+  componentDidMount() {
+    this.timerID = setInterval(() => this.updateTime(), 50);
+  }
 
-  return (
-    <div className='countdown'>
-      <Card time="days" countdown={countdown.days} />
-      <Card time="hours" countdown={countdown.hours} />
-      <Card time="minutes" countdown={countdown.minutes} />
-      <Card time="seconds" countdown={countdown.seconds} />
-    </div>
-  );
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  updateTime() {
+    // get countdown from end date - current time
+    const time = +this.props.date - +new Date();
+    // set time units
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((time / 1000 / 60) % 60);
+    const seconds = Math.floor((time / 1000) % 60);
+    // on day chanage, update days and shuffle state
+    if (days !== this.state.days) {
+      const daysShuffle = !this.state.daysShuffle;
+      this.setState({ days, daysShuffle });
+    }
+    // on hour chanage, update hours and shuffle state
+    if (hours !== this.state.hours) {
+      const hoursShuffle = !this.state.hoursShuffle;
+      this.setState({ hours, hoursShuffle });
+    }
+    // on minute chanage, update minutes and shuffle state
+    if (minutes !== this.state.minutes) {
+      const minutesShuffle = !this.state.minutesShuffle;
+      this.setState({ minutes, minutesShuffle });
+    }
+    // on second chanage, update seconds and shuffle state
+    if (seconds !== this.state.seconds) {
+      const secondsShuffle = !this.state.secondsShuffle;
+      this.setState({ seconds, secondsShuffle });
+    }
+  }
+
+  render() {
+    // state object destructuring
+    const { days, hours, minutes, seconds, daysShuffle, hoursShuffle, minutesShuffle, secondsShuffle } = this.state;
+
+    return (
+      <div className={'flipClock'}>
+        <CountdownCard unit={'days'} digit={days} shuffle={daysShuffle} />
+        <CountdownCard unit={'hours'} digit={hours} shuffle={hoursShuffle} />
+        <CountdownCard unit={'minutes'} digit={minutes} shuffle={minutesShuffle} />
+        <CountdownCard unit={'seconds'} digit={seconds} shuffle={secondsShuffle} />
+      </div>
+    );
+  }
 }
+
+export default Countdown;
---
+++ src/components/Countdown/CountdownCard.jsx
@@ -0,0 +1,46 @@
+import './CountdowdCard.scss';
+
+// function component
+const AnimatedCard = ({ animation, digit }) => {
+  return(
+    <div className={`flipCard ${animation}`}>
+      <span>{digit}</span>
+    </div>
+  );
+};
+
+// function component
+const StaticCard = ({ position, digit }) => {
+  return(
+    <div className={position}>
+      <span>{digit}</span>
+    </div>
+  );
+};
+
+export default function CountdownCard({ digit, shuffle, unit }) {
+  // assign digit values
+  let currentDigit = digit;
+  let previousDigit = digit + 1;
+
+  // to prevent a negative value
+  if (unit !== 'hours') previousDigit = previousDigit === -1 ? 59 : previousDigit;
+  else previousDigit = previousDigit === -1 ? 23 : previousDigit;
+
+  // shuffle digits
+  const digit1 = shuffle ? previousDigit : currentDigit;
+  const digit2 = !shuffle ? previousDigit : currentDigit;
+
+  // shuffle animations
+  const animation1 = shuffle ? 'fold' : 'unfold';
+  const animation2 = !shuffle ? 'fold' : 'unfold';
+
+  return (
+    <div className={'flipUnitContainer'}>
+      <StaticCard position={'upperCard'} digit={currentDigit} />
+      <StaticCard position={'lowerCard'} digit={previousDigit} />
+      <AnimatedCard digit={digit1} animation={animation1} />
+      <AnimatedCard digit={digit2} animation={animation2} />
+    </div>
+  );
+}
