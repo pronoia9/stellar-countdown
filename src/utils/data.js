3d6fd3eee11b4e2935c97b63e79a744b2bf6bb03
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-13 21:05:28 +0300

    update socials/logos data

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,7 +2,11 @@ import facebook from '/public/icon-facebook.svg';
 import instagram from '/public/icon-instagram.svg';
 import pinterest from '/public/icon-pinterest.svg';
 
-export const logos = [facebook, pinterest, instagram];
+export const socials = [
+  { url: 'http://facebook.com/', logo: facebook },
+  { url: 'http://pinterest.com/', logo: pinterest },
+  { url: 'http://instagram.com/', logo: instagram },
+];
 
 export const footer = {
   url: 'https://github.com/pronoia9',
