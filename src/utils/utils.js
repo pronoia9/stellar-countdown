b46446e150b786b45c5512b82c9c57b2e5817a23
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-14 00:23:45 +0300

    move countdown functions to utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,29 @@
 export const rem = (px) => `${px / 16}rem`;
+
+export const calculateDays = (targetDate) => Math.floor((+targetDate - +new Date()) / (1000 * 60 * 60 * 24));
+export const calculateHours = (targetDate) => Math.floor(((+targetDate - +new Date()) / (1000 * 60 * 60)) % 24);
+export const calculateMinutes = (targetDate) => Math.floor(((+targetDate - +new Date()) / 1000 / 60) % 60);
+export const calculateSeconds = (targetDate) => Math.floor(((+targetDate - +new Date()) / 1000) % 60);
+
+export const calculateCountdown = (targetDate) => ({
+  days: calculateDays(targetDate),
+  hours: calculateHours(targetDate),
+  minutes: calculateMinutes(targetDate),
+  seconds: calculateSeconds(targetDate),
+});
+
+export const updateCountdown = (targetDate, type) => {
+  console.log('update', type);
+  switch (type) {
+    case 'days':
+      return calculateDays(targetDate);
+    case 'hours':
+      return calculateHours(targetDate);
+    case 'minutes':
+      return calculateMinutes(targetDate);
+    case 'seconds':
+      return calculateSeconds(targetDate);
+    default:
+      break;
+  }
+};
