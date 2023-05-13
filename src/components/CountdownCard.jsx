1ceaa413044f91d6272cb016c5732948558fb9f0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-13 23:05:15 +0300

    add more styles

--- src/components/CountdownCard.jsx
+++ src/components/CountdownCard.jsx
@@ -10,32 +10,20 @@ const CountdownCard = ({ number, type }) => {
   );
 };
 
-/* 
-<div class='countdown__parent'>
-    <div class='countdown__top'>
-        <div></div>
-    </div>
-  
-    <div class='countdown__bottom'></div>
-  
-    <p class='countdown__text'>days</p>
-</div> 
-*/
-
 export default CountdownCard;
 
 const Container = styled.div`
+  height: clamp(4.125rem, 11vw, 8.625rem);
+  width: clamp(4.375, 11vw, 9, 25rem);
+  position: relative;
+  perspective: 31.25rem;
+  transform-style: preserve-3d;
   color: var(--color-primary-red);
   font-size: clamp(2.25rem, 7vw, 5rem);
-  height: clamp(4.125rem, 11vw, 8.625rem);
-  perspective: 31.25rem;
-  position: relative;
-  transform-style: preserve-3d;
-  width: clamp(4.375, 11vw, 9, 25rem);
 
   &::before {
-    content: '';
-    background-color: #1a1a24; /* #141419 */
+    content: ${(props) => props.number};
+    background-color: 'green'; /* #141419 */
     border-radius: 0.5rem;
     height: 100%;
     position: absolute;
@@ -44,17 +32,30 @@ const Container = styled.div`
   }
 `;
 
-const Top = styled.div``;
+const Top = styled.div`
+  width: 100%;
+  height: 50%;
+  position: relative;
 
-const Bottom = styled.div``;
+  &::before {
+    content: attr();
+    z-index: 5;
+  }
+`;
+
+const Bottom = styled.div`
+  width: 100%;
+  height: 50%;
+  position: relative;
+`;
 
 const Text = styled.div`
+  width: 100%;
+  position: absolute;
   bottom: -3.5rem;
   color: var(--color-primary-blue);
   font-size: clamp(0.5rem, 1.1vw, 0.875rem);
   letter-spacing: clamp(2px, 0.45vw, 0.5rem);
-  position: absolute;
   text-align: center;
   text-transform: uppercase;
-  width: 100%;
 `;
