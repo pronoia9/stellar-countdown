a10aa6face5cb60f5df482a33dc147ddadf19a12
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-13 21:16:26 +0300

    finish initial socials component

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,6 +1,29 @@
-import { logos } from '../utils/data';
+import styled from 'styled-components';
+import { socials } from '../utils/data';
+
+const Logo = ({ title, url, logo }) => (
+  <ListItem>
+    <a href={url} target='_blank'>
+      <img src={logo} alt={`logo-${title}`} />
+    </a>
+  </ListItem>
+);
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <Container>
+      <List>
+        {socials.map((data, i) => (
+          <Logo key={`socials-${i}`} {...data} />
+        ))}
+      </List>
+    </Container>
+  );
 };
 export default Socials;
+
+const Container = styled.div``;
+
+const List = styled.ul``;
+
+const ListItem = styled.li``;
