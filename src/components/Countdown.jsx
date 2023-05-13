25cedd8250d107e85e3be520ed0f490f0fae3464
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-13 22:32:40 +0300

    add init countdown with hardcoded numbers

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,4 +1,22 @@
+import styled from 'styled-components';
+import CountdownCard from './CountdownCard';
+
 const Countdown = () => {
-  return <div>Countdown</div>;
+  return (
+    <Container>
+      {[8, 23, 55, 41].map((data, index) => (
+        <CountdownCard key={`${['day', 'hour', 'minute', 'second'][index]}-card`} number={data} />
+      ))}
+    </Container>
+  );
 };
 export default Countdown;
+
+//countdown__container
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 2rem;
+  min-width: clamp(20.625rem, 51vw, 43rem);
+`;
