4b361cf09e119818e9b5761b9da6425d1974479b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-13 16:27:23 +0300

    clear initial data

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div class='attribution'>
+      Challenge by{' '}
+      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+        Frontend Mentor
+      </a>
+      . Coded by <a href='#'>Your Name Here</a>.
+    </div>
+  );
+};
+export default Footer;
