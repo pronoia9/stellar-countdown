0ad5f90066e1c08067ec6d56590f561de9000ce7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-13 16:41:44 +0300

    add data to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
-const Footer = () => {
+import { footer } from '../data/data';
+
+export default function Footer() {
+  const { url, name, challenge } = footer;
+
   return (
     <div class='attribution'>
-      Challenge by{' '}
-      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+      Challenge by
+      <a href={challenge} target='_blank'>
         Frontend Mentor
       </a>
-      . Coded by <a href='#'>Your Name Here</a>.
+      . Coded by <a href={url}>{name}</a>.
     </div>
   );
-};
-export default Footer;
+}
