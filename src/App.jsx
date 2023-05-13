bb56e61d59b05b11468fb9265aa79e0b2cf214b3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-13 16:51:35 +0300

    add bg + init structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,25 @@ import { Footer } from './components/';
 
 const App = () => {
   return (
-    <Container>
-      App
+    <AppContainer>
+      <ContentContainer>
+        <Title></Title>
+        <ContentWrapper></ContentWrapper>
+      </ContentContainer>
       <Footer />
-    </Container>
+    </AppContainer>
   );
 };
 export default App;
 
-const Container = styled.div``;
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: url(/pattern-hills.svg) bottom center no-repeat, url(/bg-stars.svg) no-repeat, linear-gradient(to bottom, #1e1f29, #231d2b);
+`;
+
+const ContentContainer = styled.div``;
+
+const Title = styled.div``;
+
+const ContentWrapper = styled.div``;
