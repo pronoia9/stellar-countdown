ee122cb72457f299957212ee06fdf21a1a896a28
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-13 16:35:45 +0300

    add data

---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+import facebook from '/public/icon-facebook.svg';
+import instagram from '/public/icon-instagram.svg';
+import pinterest from '/public/icon-pinterest.svg';
+
+export const logos = [facebook, pinterest, instagram];
+
+export const footer = { url: 'https://github.com/pronoia9', name: 'Jay Ansin' };
