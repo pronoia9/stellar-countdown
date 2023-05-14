863f646e5793a0a348596dec100e87aedb026e09
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-14 03:43:47 +0300

    move socials to footer + restructure init layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,25 @@
+import styled from 'styled-components';
+
+import Socials from './Socials';
 import { footer } from '../utils/data';
 
 export default function Footer() {
   const { url, name, challenge } = footer;
 
   return (
-    <div className='attribution'>
-      Challenge by
-      <a href={challenge} target='_blank'>
-        Frontend Mentor
-      </a>
-      . Coded by <a href={url}>{name}</a>.
-    </div>
+    <Container>
+      <Socials />
+      <Attribution>
+        Challenge by
+        <a href={challenge} target='_blank'>
+          Frontend Mentor
+        </a>
+        . Coded by <a href={url}>{name}</a>.
+      </Attribution>
+    </Container>
   );
 }
+
+const Container = styled.div``;
+
+const Attribution = styled.div``;
