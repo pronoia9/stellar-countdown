008ddfb4bd53d6a9a7c5f9ca6c5552ce5b275a7b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-15 00:55:57 +0300

    fix style import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import '@pqina/flip/dist/flip.min.css';
 import './reset.css';
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
