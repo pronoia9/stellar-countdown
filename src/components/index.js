17e5a80a55592827824567050c5b640761af4f4b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-14 19:57:31 +0300

    add flip component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Countdown from './Countdown';
 import CountdownCard from './CountdownCard';
+import { Flip } from './Flip';
 import Footer from './Footer';
 import Socials from './Socials';
 
-export { Countdown, CountdownCard, Footer, Socials };
+export { Countdown, CountdownCard, Flip, Footer, Socials };
