f165845a4229818106f853962e80571d492ed579
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-14 03:42:13 +0300

    restructure components (move title + change flexs)

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ import { rem } from './utils/utils';
 const App = () => {
   return (
     <AppContainer>
-      <Title>We're launching soon</Title>
       <Countdown />
-      <Socials />
-      {/* <Footer /> */}
+      <Footer />
     </AppContainer>
   );
 };
@@ -21,17 +19,9 @@ const AppContainer = styled.div`
   background: url(/src/assets/pattern-hills.svg) no-repeat bottom center, url(/src/assets/bg-stars.svg) center center,
     linear-gradient(to bottom, var(--color-neutral-blackBlue), var(--color-bg-gradient));
   background-size: contain;
-  display: grid;
-  grid-template-rows: repeat(3, auto);
-  justify-content: center;
-  justify-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   padding: 8.5rem clamp(1.5rem, 5vw, 5rem) 2rem;
-`;
-
-const Title = styled.h1`
-  color: var(--color-neutral-white);
-  text-transform: uppercase;
-  font-size: clamp(${rem(20)}, 2.5vw, 1.375rem);
-  letter-spacing: 0cap.5rem;
-  text-align: center;
 `;
--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 import CountdownCard from './CountdownCard';
 import { getTargetDate } from '../utils/data';
-import { calculateCountdown } from '../utils/utils';
+import { calculateCountdown, rem } from '../utils/utils';
 
 const Countdown = () => {
   const targetDate = getTargetDate();
@@ -11,9 +11,12 @@ const Countdown = () => {
 
   return (
     <Container>
-      {Object.values(countdown).map((data, index) => (
-        <CountdownCard targetDate={targetDate} key={`${Object.keys(countdown)[index]}-card`} number={data} type={Object.keys(countdown)[index]} />
-      ))}
+      <Title>We're launching soon</Title>
+      <Wrapper>
+        {Object.values(countdown).map((data, index) => (
+          <CountdownCard targetDate={targetDate} key={`${Object.keys(countdown)[index]}-card`} number={data} type={Object.keys(countdown)[index]} />
+        ))}
+      </Wrapper>
     </Container>
   );
 };
@@ -22,8 +25,22 @@ export default Countdown;
 //countdown__container
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-between;
   margin-bottom: 2rem;
   min-width: clamp(20.625rem, 51vw, 43rem);
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Title = styled.h1`
+  color: var(--color-neutral-white);
+  text-transform: uppercase;
+  font-size: clamp(${rem(20)}, 2.5vw, 1.375rem);
+  letter-spacing: 0cap.5rem;
+  text-align: center;
 `;
