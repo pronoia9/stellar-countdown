6809f080aa0478aeacdf10204f9e561c2e8b7968
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-16 16:39:35 +0300

    move files and folders + fix paths

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 import '@pqina/flip/dist/flip.min.css';
-import './reset.css';
-import './index.scss';
+import './styles/reset.css';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
