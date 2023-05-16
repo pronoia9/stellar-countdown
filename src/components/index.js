cea4546b4d3d7d608a422df028e5235ef8b4ceab
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-16 19:56:47 +0300

    add import/export for new component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Countdown from './Countdown';
-import CountdownCards from './CountdownCards';
+import CountdownCard from './CountdownCard';
 import Footer from './Footer';
 import Socials from './Socials';
 
-export { Countdown, CountdownCards, Footer, Socials };
+export { Countdown, CountdownCard, Footer, Socials };
